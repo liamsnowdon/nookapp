b1b0c1a7710a3132a102bdb06447126a02a6823e
Liam Snowdon <snowdon.liam@gmail.com>
2020-08-01 04:54:49 +0100

    #8: add checkbox on list items for quick add

--- src/Fossils/components/ListItem.vue
+++ src/Fossils/components/ListItem.vue
@@ -1,23 +1,43 @@
 <template>
-  <button
-    class="list-item"
-    @click="onClick"
-  >
-    <div class="list-item__inner">
-      <div class="list-item__status">
-        <i v-if="isDonated"
-           class="fa fa-check-circle"
-        />
-        <i v-else
-           class="fa fa-times-circle"
-        />
-      </div>
+  <div class="list-item-cont">
+    <button
+      class="list-item"
+      @click="onClick"
+    >
+      <div class="list-item__inner">
+        <div class="list-item__status">
+          <i v-if="isDonated"
+             class="fa fa-check-circle"
+          />
+          <i v-else
+             class="fa fa-times-circle"
+          />
+        </div>
 
-      <div class="list-item__text">
-        {{ fossil.name['name-EUen'] }}
+        <div class="list-item__text">
+          {{ fossil.name['name-EUen'] }}
+        </div>
+      </div>
+    </button>
+
+    <div class="list-item__checkbox">
+      <div class="c-checkbox c-checkbox--small">
+        <input
+          :id="`list-item-donated-${fossil['file-name']}`"
+          v-model="donated"
+          type="checkbox"
+          class="c-checkbox__input"
+          @change="onDonatedChange"
+        />
+        <label
+          :for="`list-item-donated-${fossil['file-name']}`"
+          class="c-checkbox__label"
+        >
+          <span class="c-checkbox__checkbox"></span>
+        </label>
       </div>
     </div>
-  </button>
+  </div>
 </template>
 
 <script>
@@ -34,6 +54,16 @@ export default {
     },
   },
 
+  data () {
+    return {
+      donated: false,
+    };
+  },
+
+  created () {
+    this.donated = this.isDonated;
+  },
+
   computed: {
     ...mapGetters(MODULE, [
       GETTERS.GET_DONATED_FOSSIL,
@@ -44,27 +74,48 @@ export default {
     },
   },
 
+  watch: {
+    isDonated (newValue) {
+      this.donated = newValue;
+    },
+  },
+
   methods: {
     ...mapMutations(MODULE, [
       MUTATIONS.SET_SELECTED_FOSSIL,
       MUTATIONS.SET_DETAIL_MODAL_OPEN,
+      MUTATIONS.SET_DONATED_FOSSIL_STATUS
     ]),
 
     onClick () {
       this.setSelectedFossil(this.fossil);
       this.setDetailModalOpen(true);
     },
+
+    onDonatedChange () {
+      const payload = {
+        id: this.fossil['file-name'],
+        isDonated: this.donated,
+      };
+
+      this.setDonatedFossilStatus(payload);
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+  .list-item-cont {
+    display: flex;
+    align-items: center;
+    border-top: 2px solid black;
+  }
+
   .list-item {
     @extend %button-reset;
     width: 100%;
     padding: 0 20px;
     height: 40px;
-    border-top: 2px solid black;
     color: white;
     text-align: left;
     text-transform: capitalize;
@@ -72,6 +123,10 @@ export default {
     &__inner {
       display: flex;
       align-items: center;
+    }
+
+    &__checkbox {
+      margin-left: auto;
     }
 
     .fa {
